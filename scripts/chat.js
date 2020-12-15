58b6415c96243ba70320b356f0897251a95e2b3a
Segni Adeba <se.segni.adeba@gmail.com>
2020-12-15 11:04:43 +0400

    Chatroom class and adding chats to the DB done.

---
+++ scripts/chat.js
@@ -0,0 +1,29 @@
+// add new chat documents
+// setting up a real-time listner to get new chats
+// updating the username
+// updating the room
+
+class Chatroom{
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+    }
+
+    async addChat(message){
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            message,                        // ES6 shorhand for message: message
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        }
+
+        // save the chat document
+        const response = await this.chats.add(chat);
+        return response;
+    }
+    
+}
+
